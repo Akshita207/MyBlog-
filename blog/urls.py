39f59bda20190ce49ad0445e2b99e3df7d073b9b
Akshita <akshitajain207@gmail.com>
2020-12-04 22:48:37 +0530

    pathtourl2

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
-from django.urls import path
 from blog import views
 
 urlpatterns=[
